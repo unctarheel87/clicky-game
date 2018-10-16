1437ab5c0039c23ab86c0ccb0544f6fd1f55867d
John Tillman <john.w.tillman@gmail.com>
2018-10-15 21:44:02 -0400

    Update typewriter.js

--- src/typewriter.js
+++ src/typewriter.js
@@ -1,10 +1,10 @@
 export default(() => {
   let i = 0
-  const txt = `Click any image to play! Click on an image to earn points, but don't click on any more than once!`
+  const txt = `Click any image to play!\n Click on an image to earn points, but don't click on any more than once!`
   const speed = 60;
   const typeWriter = () => {
     if (i < txt.length) {
-      document.getElementById("headerText").innerHTML += txt.charAt(i);
+      document.getElementById("headerText").textContent += txt.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
     }
